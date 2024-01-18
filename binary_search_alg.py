2e2d68d0e5e76993f7a5b763f27294cc831e973a
argamManukyan <62460275+argamManukyan@users.noreply.github.com>
2024-01-18 19:54:49 +0400

    binsearch

---
+++ binary_search_alg.py
@@ -0,0 +1,24 @@
+arr = [1, 2, 3, 4, 7, 11, 19]
+
+
+def binsearch(elements: list[int], target: int) -> int:
+    """Returns an index of found element"""
+
+    left = 0
+    right = len(elements) - 1
+
+    while left <= right:
+        mididx = (left + right) // 2
+
+        if elements[mididx] < target:
+            left = mididx + 1
+        elif elements[mididx] > target:
+            right = mididx - 1
+        else:
+            return mididx
+
+    return False
+
+
+res = binsearch(arr, 11)
+print(res)
