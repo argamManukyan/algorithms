646098c32e18373c4e5288b0698b272ef3783ebe
argamManukyan <62460275+argamManukyan@users.noreply.github.com>
2024-01-18 22:13:25 +0400

    find two elements sum (having a target)

---
+++ exercises/two_sum.py
@@ -0,0 +1,26 @@
+"""Find the numbers sum of which will be equal to the target number"""
+
+
+def find_two_sum(num_list: list[int], target: int):
+
+    left = 0
+    right = len(num_list) - 1
+
+    while left < right:
+        while left < right and num_list[left] + num_list[right] > target:
+            right -= 1
+
+        while left < right and num_list[left] + num_list[right] < target:
+            left += 1
+
+        if num_list[left] + num_list[right] == target:
+            return [num_list[left], num_list[right]]
+
+    return False
+
+
+if __name__ == "__main__":
+    target = 18
+    nums = [1, 2, 8, 10, 11]
+    res = find_two_sum(nums, target)
+    print(res)
